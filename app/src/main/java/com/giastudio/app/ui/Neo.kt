f6c01f94925d1ece03474a373b29ed2e149d6fd3
Samuel Mensah <alphariansamuel@gmail.com>
2026-09-03 21:09:49 +0000

    Update app/src/main/java/com/giastudio/app/ui/Neo.kt

--- app/src/main/java/com/giastudio/app/ui/Neo.kt
+++ app/src/main/java/com/giastudio/app/ui/Neo.kt
@@ -19,7 +19,7 @@ import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.shape.RectangleShape
+import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.HelpOutline
 import androidx.compose.material3.Icon
